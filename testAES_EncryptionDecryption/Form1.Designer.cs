40870113bf99ee9dec9593cc33a2a051aa646e33
OpenAI <OpenAI@CHATGPT>
2023-07-21 16:30:48 +0500

    Add project files.

---
+++ testAES_EncryptionDecryption/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace testAES_EncryptionDecryption
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPasswordEncrypt = new System.Windows.Forms.TextBox();
+            this.txtPasswordDecrypt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(180, 58);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt File";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(180, 115);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "Decrypt File";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Password then click on Encrypt to select file";
+            // 
+            // txtPasswordEncrypt
+            // 
+            this.txtPasswordEncrypt.Location = new System.Drawing.Point(15, 58);
+            this.txtPasswordEncrypt.Name = "txtPasswordEncrypt";
+            this.txtPasswordEncrypt.Size = new System.Drawing.Size(153, 20);
+            this.txtPasswordEncrypt.TabIndex = 3;
+            // 
+            // txtPasswordDecrypt
+            // 
+            this.txtPasswordDecrypt.Location = new System.Drawing.Point(15, 115);
+            this.txtPasswordDecrypt.Name = "txtPasswordDecrypt";
+            this.txtPasswordDecrypt.Size = new System.Drawing.Size(153, 20);
+            this.txtPasswordDecrypt.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 199);
+            this.Controls.Add(this.txtPasswordDecrypt);
+            this.Controls.Add(this.txtPasswordEncrypt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AES Test (Enryption and Decryption)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPasswordEncrypt;
+        private System.Windows.Forms.TextBox txtPasswordDecrypt;
+    }
+}
+
---
+++ testAES_EncryptionDecryption/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace testAES_EncryptionDecryption
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] EncryptFile(string filePath, string password)
+        {
+            byte[] encryptedData;
+            using (Aes aesAlg = Aes.Create())
+            {
+                byte[] key = new byte[32];
+                byte[] iv = new byte[16];
+
+                using (Rfc2898DeriveBytes keyDerivationFunction = new Rfc2898DeriveBytes(password, iv))
+                {
+                    key = keyDerivationFunction.GetBytes(32);
+                    iv = keyDerivationFunction.GetBytes(16);
+                }
+
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                byte[] dataToEncrypt = File.ReadAllBytes(filePath);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (ICryptoTransform encryptor = aesAlg.CreateEncryptor())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            csEncrypt.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                        }
+                    }
+                    encryptedData = msEncrypt.ToArray();
+                }
+            }
+
+            return encryptedData;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFilePath = openFileDialog.FileName;
+                    string password = txtPasswordEncrypt.Text;
+
+                    try
+                    {
+                        byte[] encryptedData = EncryptFile(selectedFilePath, password);
+                        string encryptedFilePath = selectedFilePath + ".encrypted";
+
+                        File.WriteAllBytes(encryptedFilePath, encryptedData);
+
+                        MessageBox.Show("File encrypted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error encrypting the file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private byte[] DecryptFile(string encryptedFilePath, string password)
+        {
+            byte[] decryptedData;
+            using (Aes aesAlg = Aes.Create())
+            {
+                byte[] key = new byte[32];
+                byte[] iv = new byte[16];
+
+                // Generate random key and IV
+                using (Rfc2898DeriveBytes keyDerivationFunction = new Rfc2898DeriveBytes(password, iv))
+                {
+                    key = keyDerivationFunction.GetBytes(32);
+                    iv = keyDerivationFunction.GetBytes(16);
+                }
+
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                // Read the encrypted file into a byte array
+                byte[] dataToDecrypt = File.ReadAllBytes(encryptedFilePath);
+
+                // Create a memory stream to hold the decrypted data
+                using (MemoryStream msDecrypt = new MemoryStream())
+                {
+                    using (ICryptoTransform decryptor = aesAlg.CreateDecryptor())
+                    {
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
+                        {
+                            // Write the decrypted data to the memory stream
+                            csDecrypt.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                        }
+                    }
+                    decryptedData = msDecrypt.ToArray();
+                }
+            }
+
+            return decryptedData;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Encrypted Files|*.encrypted";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string encryptedFilePath = openFileDialog.FileName;
+                    string password = txtPasswordDecrypt.Text;
+
+                    try
+                    {
+                        byte[] decryptedData = DecryptFile(encryptedFilePath, password);
+                        string originalFilePath = encryptedFilePath.Substring(0, encryptedFilePath.LastIndexOf(".encrypted"));
+
+                        File.WriteAllBytes(originalFilePath, decryptedData);
+
+                        MessageBox.Show("File decrypted and saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error decrypting the file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
